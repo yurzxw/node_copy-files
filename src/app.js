ab12665cb9d2b7577bd8eaebff6b29bf6c60328e
Yura Dziavoruk <yurzxw@MacBook-Air-Yura.local>
2025-05-21 14:53:19 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,30 @@
 'use strict';
+import fs from 'fs';
+
+const copyFile = () => {
+  try {
+    const params = process.argv.slice(2);
+    const file = params[0];
+    const fileCopy = params[1];
+
+    if (!file || !fileCopy) {
+      return;
+    }
+
+    fs.readFile(file, 'utf8', (error, data) => {
+      if (error) {
+        return error.message;
+      }
+
+      fs.writeFile(fileCopy, data, 'utf8', (err) => {
+        if (err) {
+          return err.message;
+        }
+      });
+    });
+  } catch (e) {
+    return e.message;
+  }
+};
+
+copyFile();
